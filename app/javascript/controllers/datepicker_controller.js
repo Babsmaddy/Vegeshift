355ff48ffd779a81c086343585949d9eaf4d4513
xavier bahegne <xavierbahegne@gmail.com>
2024-11-27 18:03:20 +0100

    add javascript controller

---
+++ app/javascript/controllers/datepicker_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+// Connects to data-controller="datepicker"
+export default class extends Controller {
+  static targets = ["button"]
+  connect() {
+    console.log("hello from datepicker controller");
+    //
+    console.log(this.buttonTarget);
+
+  }
+}
