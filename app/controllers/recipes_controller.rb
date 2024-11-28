ef6ea03cccd49efe6bab9748b1d629f0fe1703cf
najoua lamtioui <najoua-lamtioui@hotmail.fr>
2024-11-28 18:14:13 +0100

    done

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,11 +15,31 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new
+
+    @recipe = Recipe.new(recipe_params)
     if @recipe.save
+      @recipe.sum_total_co2
       redirect_to recipes_path
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if recipe.update(recipe_params)
+
+      @recipe.sum_total_co2
+      redirect_to recipes_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :difficulty, :time, :co2)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,12 @@ class Recipe < ApplicationRecord
   has_many :steps
   has_many :ingredients, through: :recipe_ingredients
   has_one_attached :photo
+
+  def sum_total_co2
+    # self.ingredients.sum {|ingredient| ingredient.co2 || 100}
+    return if ingredients.blank?
+
+    total_co2 = ingredients.sum { |ingredient| ingredient.co2 || 100 }
+    update(co2: total_co2)
+  end
 end
