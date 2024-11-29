f60dbec798773aa506abda1759a852a2c2d43cb2
xavier bahegne <xavierbahegne@gmail.com>
2024-11-29 11:31:47 +0100

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -53,7 +53,8 @@ class Recipe < ApplicationRecord
       )
     end
     @recipe
-
+  end
+  
   def sum_total_co2
     # self.ingredients.sum {|ingredient| ingredient.co2 || 100}
     return if ingredients.blank?
