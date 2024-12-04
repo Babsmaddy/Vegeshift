68e152e02987ee9d91b9d30917e642f0bfa77b3c
xavier bahegne <xavierbahegne@gmail.com>
2024-12-04 12:14:19 +0100

    add the chart

---
+++ app/javascript/controllers/chart_co2_impact_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="chart-co2-impact"
+export default class extends Controller {
+  connect() {
+    const worldPopulation = {
+      men: 504,
+      women: 496
+    };
+    const doughnutLabels = Object.keys(worldPopulation);
+    const doughnutSata = Object.values(worldPopulation);
+
+    const doughnut = new Chart(this.element, {
+      type: 'doughnut',
+      data: {
+        labels: doughnutLabels,
+        datasets: [{
+          label: 'Word Population',
+          data: doughnutSata,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
+  }
+}
