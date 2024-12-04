a826a93ab36d019e4b1507c3eabb5b8fde2a5c51
xavier bahegne <xavierbahegne@gmail.com>
2024-12-04 16:12:59 +0100

    still trying to config

--- app/javascript/controllers/chart_co2_impact_controller.js
+++ app/javascript/controllers/chart_co2_impact_controller.js
@@ -1,33 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
-// import { Chart } from "chart.js";
+import Chart from '@stimulus-components/chartjs'
 
 // Connects to data-controller="chart-co2-impact"
 export default class extends Controller {
   connect() {
     console.log("hello");
 
-    // const worldPopulation = {
-    //   men: 504,
-    //   women: 496
-    // };
-    // const doughnutLabels = Object.keys(worldPopulation);
-    // const doughnutSata = Object.values(worldPopulation);
+    const worldPopulation = {
+      men: 504,
+      women: 496
+    };
+    const doughnutLabels = Object.keys(worldPopulation);
+    const doughnutSata = Object.values(worldPopulation);
 
-    // const doughnut = new Chart(this.element, {
-    //   type: 'doughnut',
-    //   data: {
-    //     labels: doughnutLabels,
-    //     datasets: [{
-    //       label: 'Word Population',
-    //       data: doughnutSata,
-    //       backgroundColor: [
-    //         'rgb(255, 99, 132)',
-    //         'rgb(54, 162, 235)',
-    //         'rgb(255, 205, 86)'
-    //       ],
-    //       hoverOffset: 4
-    //     }]
-    //   }
-    // });
+    const doughnut = new Chart(this.element, {
+      type: 'doughnut',
+      data: {
+        labels: doughnutLabels,
+        datasets: [{
+          label: 'Word Population',
+          data: doughnutSata,
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    });
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,16 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-// import { Chart } from "./chart.js";
-// import * as Chartjs from "./chart.js";
+// import { Chart } from "chart.js";
+// import * as Chartjs from "chart.js";
+import { Application } from '@hotwired/stimulus'
+import Chart from '@stimulus-components/chartjs'
 
 eagerLoadControllersFrom("controllers", application)
+
+let application = Application.start()
+application.register('chart', Chart)
+
 
 // const controllers = Object.values(Chartjs).filter(
 //   (chart) => chart.id !== undefined
