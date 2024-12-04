101fce65e2568f0b11e43d64eb3669b5302c08f2
xavier bahegne <xavierbahegne@gmail.com>
2024-12-04 15:48:20 +0100

    update seed

--- app/services/generate_recipe_gpt.rb
+++ app/services/generate_recipe_gpt.rb
@@ -21,8 +21,9 @@ class GenerateRecipeGpt
   def photo_gpt(upload)
     text = "
     Je veux que tu prennes le nom de cette recette et que tu me la retourne en version végétale avec ce format :
-    { name: nom de la recette végétalisé, time: .., difficulty: (entre 1 et 3), cost: (entre 1 et 3), vegetal: true, co2: 0, ingredients: [{ name: nom de l'ingrédient, quantity: quantité en gramme }, { ... }],
-    steps: [{ number: numéro de l'étape, content: contenu de l'étape }, { ... }] }
+      { name: nom de la recette végétalisé, time: .., difficulty: (entre 1 et 3), cost: (entre 1 et 3), vegetal: true, co2: 0, co2_traditional: estime le cout en co2 en gramme de la recette traditionnel, ingredients: [{ name: nom de l'ingrédient, quantity: quantité en gramme }, { ... }],
+      steps: [{ number: numéro de l'étape, content: contenu de l'étape }, { ... }] }
+      Je souhaiterais au moins 4 ingrédients et au moins 5 étapes.
     S'il te plait, donne moi uniquement le JSON prêt à être parser dans une méthode de classe Ruby. Enlève le ```json au début de ta réponse et rien d'autre comme 'voici la recette ..'. Je compte sur toi.
     Merci
     "
@@ -51,7 +52,7 @@ class GenerateRecipeGpt
           "
           Voici un paramètre : #{upload}
           avec ce paramètre je veux que tu prennes le nom de cette recette et que tu me la retourne en version végétale avec ce format :
-          { name: nom de la recette végétalisé, time: .., difficulty: (entre 1 et 3), cost: (entre 1 et 3), vegetal: true, co2: 0, ingredients: [{ name: nom de l'ingrédient, quantity: quantité en gramme }, { ... }],
+          { name: nom de la recette végétalisé, time: .., difficulty: (entre 1 et 3), cost: (entre 1 et 3), vegetal: true, co2: 0, co2_traditional: estime le cout en co2 en gramme de la recette traditionnel, ingredients: [{ name: nom de l'ingrédient, quantity: quantité en gramme }, { ... }],
               steps: [{ number: numéro de l'étape, content: contenu de l'étape }, { ... }] }
           S'il te plait, donne moi uniquement le JSON prêt à être parser dans une méthode de classe Ruby. Enlève le ```json au début de ta réponse et rien d'autre comme 'voici la recette ..'. Je compte sur toi.
           Merci
@@ -67,8 +68,8 @@ class GenerateRecipeGpt
       time: gpt["time"],
       difficulty: gpt["difficulty"],
       cost: gpt["cost"],
+      co2_traditional: gpt["co2_traditional"],
       vegetal: true,
-      co2: 0
     )
 
     gpt["steps"].each do |step|
@@ -98,7 +99,7 @@ class GenerateRecipeGpt
   def new_photo_gpt
     response = @client.images.generate(
       parameters: {
-        prompt: "Can you find a beautiful popular instagram or pinterest image of the vegetarian recipe of #{@recipe.name} with the ingredients mentionned in #{@recipe.ingredients.pluck(:name).join(',')}",
+        prompt: "Je souhaiterai une image de ce plat #{@recipe.name} qui est un plate végétale. Je ne veux donc pas d'image avec de la viande. Voici la liste d'ingrédients si tu veux pour t'inspirer dans ta création #{@recipe.ingredients.pluck(:name).join(',')}. Cette photo sera destinée à être vue par mes followers sur instagram qui sont habitué à voir des close-up vue de haut de mes plats. J'utilise des filtres à la mode pour un effet réaliste mais qui donne envie d'être dégusté. Je veux uniquement la photo du plat dans une assiette cuisinée, prêt à être servi.",
         size: "256x256",
       }
     )
